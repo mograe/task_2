94654ff650f8aa8055432ed035b87feb3a6b640d
Eugene Mogran <jenamovieo@gmail.com>
2021-04-01 19:19:40 +0500

    first commit

---
+++ brackets.py
@@ -0,0 +1,48 @@
+string = input()
+req_brack = input()
+antibrack = ""
+open_brackets = ['(','[','{','<']
+close_brackets = [')',']','}','>']
+brackets = ""
+pos_brackets = {}
+stack = []
+no_error = True
+error_brack = None
+error_pos = None
+
+for i in req_brack:
+    antibrack += close_brackets[open_brackets.index(i)]
+
+for i in range(len(string)):
+    if(string[i] in open_brackets or string[i] in close_brackets):
+        brackets += string[i]
+        pos_brackets[string[i]] = i
+
+for i in brackets:
+    if (i in open_brackets and i in req_brack):
+        stack.append(i)
+    else:
+        if (i in close_brackets and stack!=[] and stack[-1]==open_brackets[close_brackets.index(i)] and i in antibrack):
+            stack.pop()
+        elif (i in antibrack):
+            stack.append(i)
+
+
+
+if(stack!=[]):
+    no_error = False
+    for i in stack:
+        if(i in close_brackets):
+            error_pos = (i, pos_brackets.get(i))
+            break
+    
+    error_brack = (stack[stack.index(error_pos[0])-1], pos_brackets.get(stack[stack.index(error_pos[0])-1]))
+    
+    
+
+print(no_error,error_pos,error_brack,sep=", ")
+
+            
+        
+
+
---
+++ magicspiral.py
@@ -0,0 +1,27 @@
+size_matrix=int(input())
+s = 0
+
+matrix = [[] for I in range(size_matrix)]
+for i in range(size_matrix):
+    for j in range(size_matrix):
+        matrix[i].append(0)
+
+for t in range(1,size_matrix+1):
+    for i in range(t-1,size_matrix-t+1):
+        s+=1
+        matrix[t-1][i] = s
+    for i in range(t,size_matrix-t+1):
+        s+=1
+        matrix[i][size_matrix-t] = s
+    for i in reversed(range(t-1,size_matrix-t)):
+        s+=1
+        matrix[size_matrix-t][i] = s
+    for i in reversed(range(t,size_matrix-t)):
+        s+=1
+        matrix[i][t-1] = s
+
+
+for i in range(size_matrix):
+    for j in range(size_matrix):
+        print(matrix[i][j],end=' ')
+    print("\n",end="") 
